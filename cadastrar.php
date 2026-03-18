ccecc24c96179e37a7e3ed120521d131de37b12c
Luís Gustavo Alves <136252324+Nessquick0998@users.noreply.github.com>
2026-03-17 23:12:18 -0300

    Update cadastrar.php

---
+++ cadastrar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cadastrar Usuário</title>
+</head>
+<body>
+<h2>Cadastrar Usuário</h2>
+<form action="salvar.php" method="POST">
+    <input type="text" name="nome" placeholder="Nome" required>
+    <input type="email" name="email" placeholder="Email" required>
+    <button type="submit">Salvar</button>
+</form>
+<a href="index.php">Ver usuários</a>
+</body>
+</html>
