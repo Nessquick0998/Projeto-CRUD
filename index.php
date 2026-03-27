f3e2045461df0967bf470e1dac3bd4e15dd12c55
Luís Gustavo Alves <totinosunited@gmail.com>
2026-03-27 20:17:28 -0300

    Alteração para edit no index

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-?>
 <?php include 'conexao.php'; ?>
 
-<h2>Usuários</h2>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Usuários</title>
+</head>
+<body>
+
+<h2>Lista de Usuários</h2>
+
 <a href="cadastrar.php">Novo usuário</a>
 
 <table border="1">
@@ -13,16 +16,26 @@ error_reporting(E_ALL);
     <th>ID</th>
     <th>Nome</th>
     <th>Email</th>
+    <th>Ações</th>
 </tr>
 
 <?php
 $result = mysqli_query($conn, "SELECT * FROM usuarios");
+
 while ($row = mysqli_fetch_assoc($result)) {
     echo "<tr>
         <td>{$row['id']}</td>
         <td>{$row['nome']}</td>
         <td>{$row['email']}</td>
+        <td>
+            <a href='editar.php?id={$row['id']}'>Editar</a> |
+            <a href='excluir.php?id={$row['id']}'>Excluir</a>
+        </td>
     </tr>";
 }
 ?>
+
 </table>
+
+</body>
+</html>
